3a9e99cf695d6f335e03ce11098a24ace43c05dc
byaswath <byaswath@gmail.com>
2024-08-12 15:22:12 +0530

    Updated Successfully

--- src/main/java/com/webApp/WebAppController.java
+++ src/main/java/com/webApp/WebAppController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebAppController {
-	@GetMapping (value = "/getHello" )
+	@GetMapping (value = "/getHelloWorld" )
 	public String getHello() {
 		return "Hello";
 	}
